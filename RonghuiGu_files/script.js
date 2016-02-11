2bbafef86c8a9c553abab8d93bca2e5662bf5648
ronghui.gu_yale.edu <ronghui.gu@yale.edu>
2016-02-11 16:35:29 -0500

    Initial

---
+++ RonghuiGu_files/script.js
@@ -0,0 +1,8 @@
+﻿/** External Links **/
+
+function externalLinks() { if (!document.getElementsByTagName) return; var anchors = document.getElementsByTagName("a"); for (var i=0; i<anchors.length; i++) { var anchor = anchors[i]; if (anchor.getAttribute("href") && anchor.getAttribute("rel") == "external") anchor.target = "_blank"; } } window.onload = externalLinks;
+
+
+/** SuckerFish Menu **/
+
+$(document).ready(function(){ $('ul.dropdown').superfish({ autoArrows: false }); });
