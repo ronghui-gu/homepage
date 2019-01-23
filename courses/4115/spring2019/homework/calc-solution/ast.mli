2f18b14d0035838d52989dce709664fdd88661b9
Ronghui Gu <guronghuieric@gmail.com>
2019-01-23 11:21:14 -0500

    Remove files

--- courses/4115/spring2019/homework/calc-solution/ast.mli
+++
@@ -1,8 +0,0 @@
-type operator = Add | Sub | Mul | Div
-
-type expr =
-    Binop of expr * operator * expr
-  | Seq of expr * expr
-  | Asn of string * expr
-  | Var of string
-  | Lit of int
--- courses/4115/spring2019/homework/hw3-1.c
+++
@@ -1,6 +0,0 @@
-int a[2][3]; /* Globals not optimized */
-int i, j;    /* Make names readable */
-
-int access() {
-  return a[i][j];
-}
